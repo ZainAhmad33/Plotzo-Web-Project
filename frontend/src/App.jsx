7742a76ba9e5c3b90d38caf05d4fb6853002d919
Zain Ahmad <zainwajid33@gmail.com>
2024-04-13 18:47:38 +0500

    Added HomePage route in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import Navbar from "./components/Navbar/Navbar"
+import HomePage from "./Pages/HomePage/HomePage"
+import Layout from "./Pages/Layout/Layout"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 function App() {
-  return (
-    <Navbar/>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path: "/",
+          element: <HomePage/>
+        }
+      ]
+    }
+  ]);
+
+  return (<RouterProvider router={router} />)
 }
+
 
 export default App
