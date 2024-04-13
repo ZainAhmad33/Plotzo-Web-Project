221b0571abb043cc7806cd25fbf1d44ee6f75114
Zain Ahmad <zainwajid33@gmail.com>
2024-04-13 18:39:30 +0500

    Created HomePage

---
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,42 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./HomePage.scss";
+// import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+//   const {currentUser} = useContext(AuthContext)
+
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+          Your gateway to finding real estate and securing your dream place. Explore our diverse listings and embark on the journey to finding the perfect home that truly reflects your aspirations and lifestyle.!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
