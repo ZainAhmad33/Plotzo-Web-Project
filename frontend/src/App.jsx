6d98cba03d1f042639b20100f0ac27e1c423e21f
Zain Ahmad <zainwajid33@gmail.com>
2024-04-13 20:02:03 +0500

    Added cards to the homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,48 +3,50 @@ import Layout from "./Pages/Layout/Layout"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { listData } from "./lib/dummydata";
 import Card from "./components/Card/Card";
+
+
 // Test Pages
-// function App() {
-//   const router = createBrowserRouter([
-//     {
-//       path: "/",
-//       element: <Layout />,
-//       children:[
-//         {
-//           path: "/",
-//           element: <HomePage/>
-//         }
-//       ]
-//     }
-//   ]);
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path: "/",
+          element: <HomePage/>
+        }
+      ]
+    }
+  ]);
 
-//   return (<RouterProvider router={router} />)
-// }
-
-// Test Components
-function App(){
- var listing =  {
-    id: 3,
-    title: "A New Apartment in the City!",
-    images: ["https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"],
-    bedroom: 1,
-    bathroom: 1,
-    price: 800,
-    address: "101 Baker Street, London",
-    latitude: 53.4808,
-    longitude: -2.2426,
-    type: "Rent"
-  }
-  console.log(listing)
-  return (
-    <>
-      <Card item={listData[0]}/>
-      <Card item={listData[2]}/>
-      <Card item={listData[3]}/>
-    </>
-
-  )
+  return (<RouterProvider router={router} />)
 }
+
+// // Test Components
+// function App(){
+//  var listing =  {
+//     id: 3,
+//     title: "A New Apartment in the City!",
+//     images: ["https://images.pexels.com/photos/2062426/pexels-photo-2062426.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"],
+//     bedroom: 1,
+//     bathroom: 1,
+//     price: 800,
+//     address: "101 Baker Street, London",
+//     latitude: 53.4808,
+//     longitude: -2.2426,
+//     type: "Rent"
+//   }
+//   console.log(listing)
+//   return (
+//     <>
+//       <Card item={listData[0]}/>
+//       <Card item={listData[2]}/>
+//       <Card item={listData[3]}/>
+//     </>
+
+//   )
+// }
 
 
 export default App
--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,40 +1,72 @@
 import { useContext } from "react";
 import SearchBar from "../../components/searchBar/SearchBar";
 import "./HomePage.scss";
+import Card from "../../components/Card/Card";
+import { listData } from "../../lib/dummydata";
 // import { AuthContext } from "../../context/AuthContext";
 
 function HomePage() {
 
 //   const {currentUser} = useContext(AuthContext)
-
+  console.log(listData)
   return (
-    <div className="homePage">
-      <div className="textContainer">
-        <div className="wrapper">
-          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-          <p>
-          Your gateway to finding real estate and securing your dream place. Explore our diverse listings and embark on the journey to finding the perfect home that truly reflects your aspirations and lifestyle.!
-          </p>
-          <SearchBar />
-          <div className="boxes">
-            <div className="box">
-              <h1>16+</h1>
-              <h2>Years of Experience</h2>
-            </div>
-            <div className="box">
-              <h1>200</h1>
-              <h2>Award Gained</h2>
-            </div>
-            <div className="box">
-              <h1>2000+</h1>
-              <h2>Property Ready</h2>
+    <div className="home-wrapper">
+
+    
+      <div className="homePage">
+        <div className="textContainer">
+          <div className="wrapper">
+            <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+            <p>
+            Your gateway to finding real estate and securing your dream place. Explore our diverse listings and embark on the journey to finding the perfect home that truly reflects your aspirations and lifestyle.!
+            </p>
+            <SearchBar />
+            <div className="boxes">
+              <div className="box">
+                <h1>16+</h1>
+                <h2>Years of Experience</h2>
+              </div>
+              <div className="box">
+                <h1>200</h1>
+                <h2>Award Gained</h2>
+              </div>
+              <div className="box">
+                <h1>2000+</h1>
+                <h2>Property Ready</h2>
+              </div>
             </div>
           </div>
         </div>
+        <div className="imgContainer">
+          <img src="/bg.png" alt="" />
+        </div>
       </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
+
+      <br/>
+      <br/>
+      <div className="properties-list">
+        <h1>Top Properties</h1>
+        <br/>
+        <br/>
+        {
+          listData.map(list => {
+            if (list.type === "Sale"){
+              return (<Card item={list}/>)
+            }
+          })}
       </div>
+      <br/>
+      <div className="properties-list">
+        <h1>Top Rental Properties</h1>
+        <br/>
+        {
+          listData.map(list => {
+            if (list.type === "Rent"){
+              return (<Card item={list}/>)
+            }
+          })}
+      </div>
+      
     </div>
   );
 }
