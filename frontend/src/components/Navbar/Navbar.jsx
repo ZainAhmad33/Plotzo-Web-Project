c49e8dfed53c1d0a527ad60991505183e3118015
Syed Hassan Haider <119353034+HassanHaider1212@users.noreply.github.com>
2024-05-05 22:42:11 +0500

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,11 +2,17 @@ import { useContext, useState } from "react";
 import "./navbar.scss";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
+import {useNotificationStore} from "../../lib/notficationStore"
 function Navbar() {
   const [open, setOpen] = useState(false);
 
   const { currentUser } = useContext(AuthContext);
 
+  const fetch = useNotificationStore((state)=> state.fetch);
+  const number = useNotificationStore((state) => state.number);
+
+  if(currentUser) fetch();
+  
   return (
     <nav>
       <div className="left">
