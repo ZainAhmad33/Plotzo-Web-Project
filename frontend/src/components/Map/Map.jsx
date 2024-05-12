2d774f2b3f81e79a72455dc00e6c6f3f0ab0c885
Syed Hassan Haider <119353034+HassanHaider1212@users.noreply.github.com>
2024-05-12 17:05:40 +0500

    Update Map.jsx

--- frontend/src/components/Map/Map.jsx
+++ frontend/src/components/Map/Map.jsx
@@ -4,13 +4,12 @@ import "leaflet/dist/leaflet.css";
 import Pin from "../Pin/Pin";
 
 function Map({ items }) {
-    console.log("Map: ", items)
   return (
     <MapContainer
       center={
-        items.length >= 1
+        items.length === 1
           ? [items[0].latitude, items[0].longitude]
-          : [30.3753, 69.3451]
+          : [52.4797, -1.90269]
       }
       zoom={7}
       scrollWheelZoom={false}
